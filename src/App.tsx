4fa35465f9cc402ce73e3c1ac2aecb58a1422fb1
miashkou <9808815@gmail.com>
2021-12-07 14:56:17 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
+import { Route, Routes} from 'react-router-dom';
+import { RootStateType} from './redux/state';
+
+type AppPropsType = {
+    state: RootStateType
+    addPost: ()=>void
+    updateNewPostText: (text:string)=>void
+}
+
+const App: React.FC<AppPropsType> = (props) => {
+
+    return (
+        <div className="app-wrapper">
+            <Header/>
+
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Routes>
+                    <Route path="/dialogs"
+                           element={<Dialogs state={props.state.dialogsPage}/>}/>
+                    <Route path="/profile"
+                           element={<Profile
+                               profilePage={props.state.profilePage}
+                               addPost={props.addPost}
+                               updateNewPostText={props.updateNewPostText}
+                           />}
+                    />
+                    <Route path="/news" element={<News/>}/>
+                    <Route path="/music" element={<Music/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
+                </Routes>
+            </div>
+        </div>
+    )
+}
+
+export default App;
